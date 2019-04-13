5f869bf39aa541936c6bc8fc17abd0664a9ca54a
liuxingwei <matchless@163.com>
2019-04-13 17:47:37 +0800

    完成 sqlite 控制器类测试示例。

---
+++ src/test/java/cn/liuxingwei/multidatasource/controller/rest/sqlite/master/UserRestControllerTest.java
@@ -0,0 +1,62 @@
+package cn.liuxingwei.multidatasource.controller.rest.sqlite.master;
+import cn.liuxingwei.multidatasource.domain.sqlite.SqliteUserDomain;
+import cn.liuxingwei.multidatasource.service.sqlite.master.SqliteMasterUserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserRestController.class)
+public class UserRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SqliteMasterUserService sqliteMasterUserService;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getMasterUserById() throws Exception {
+        Integer id = 1;
+        String userName = "张三疯";
+        String userPassword = "aaabbbccc";
+        String userCreateTime = "2019080808";
+        String userUpdateTime = "2019090909";
+
+        SqliteUserDomain user = new SqliteUserDomain();
+        user.setUserId(id);
+        user.setUserName(userName);
+        user.setUserPassword(userPassword);
+        user.setUserCreateTime(userCreateTime);
+        user.setUserUpdateTime(userUpdateTime);
+
+        Mockito.when(sqliteMasterUserService.getUserByUserId(id)).thenReturn(user);
+
+        ResultActions resultAction = mockMvc.perform(get("/user/getUserByUserId").param("user_id","1"));
+        resultAction.andExpect(jsonPath("userId").value(id))
+                .andExpect(jsonPath("userName").value(userName))
+                .andExpect(jsonPath("userPassword").value(userPassword))
+                .andExpect(jsonPath("userCreateTime").value(userCreateTime))
+                .andExpect(jsonPath("userUpdateTime").value(userUpdateTime));
+        ;
+    }
+}
---
+++ src/test/java/cn/liuxingwei/multidatasource/controller/rest/sqlite/slave/SqliteSlaveUserRestControllerTest.java
@@ -0,0 +1,62 @@
+package cn.liuxingwei.multidatasource.controller.rest.sqlite.slave;
+
+import cn.liuxingwei.multidatasource.domain.sqlite.SqliteUserDomain;
+import cn.liuxingwei.multidatasource.service.sqlite.slave.SqliteSlaveUserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SqliteSlaveUserRestController.class)
+public class SqliteSlaveUserRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SqliteSlaveUserService sqliteSlaveUserService;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getSlaveUserById() throws Exception {
+        Integer id = 1;
+        String userName = "李四海";
+        String userPassword = "aaabbbccc";
+        String userCreateTime = "2019080808";
+        String userUpdateTime = "2019090909";
+
+        SqliteUserDomain user = new SqliteUserDomain();
+        user.setUserId(id);
+        user.setUserName(userName);
+        user.setUserPassword(userPassword);
+        user.setUserCreateTime(userCreateTime);
+        user.setUserUpdateTime(userUpdateTime);
+
+        Mockito.when(sqliteSlaveUserService.getUserByUserId(id)).thenReturn(user);
+
+        ResultActions resultAction = mockMvc.perform(get("/user/slave/getUserByUserId").param("user_id", "1"));
+        resultAction.andExpect(jsonPath("userId").value(id))
+                .andExpect(jsonPath("userName").value(userName))
+                .andExpect(jsonPath("userPassword").value(userPassword))
+                .andExpect(jsonPath("userCreateTime").value(userCreateTime))
+                .andExpect(jsonPath("userUpdateTime").value(userUpdateTime));
+    }
+}
